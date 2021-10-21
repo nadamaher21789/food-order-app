195c3f31f0fca7ecc60208b557b7a0c586404fa1
Nada Maher Mahmoud <44345049+nadamaher21789@users.noreply.github.com>
2021-10-21 15:21:20 +0200

    reinit

--- src/App.js
+++ src/App.js
@@ -1,60 +1,7 @@
-
-import React, { useState } from "react";
-import ExpenseList from "./components/Expenses/ExpenseList";
-import NewExpense from "./components/NewExpense/NewExpense";
-import ExpensesArray from "./components/Expenses/ExpensesArray";
-import Plot from 'react-plotly.js';
-
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2021, 7, 14),
-  },
-  { id: 'e2', 
-  title: 'New TV', 
-  amount: 799.49, 
-  date: new Date(2021, 2, 12) 
-  },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
-
-
-const App = () =>{
-
-  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
-
-  const addExpenseHandler = expense =>{
-    setExpenses((prevExpenses)=>{
-      return [expense, ...prevExpenses];
-    });
-  };
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2',{},"Let's get started"),
-  //   React.createElement(ExpenseList,{expenses:{expenses}})
-  // );
-  
+function App() {
   return (
-    
     <div>
-    
-      <NewExpense onAddExpense = {addExpenseHandler}/>
-      
-      <ExpenseList   expenses = {expenses} />
+      <h2>Let's get started!</h2>
     </div>
   );
 }
